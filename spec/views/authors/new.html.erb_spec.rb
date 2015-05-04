241decb2d7ecfe585529479a38b6ca94afb33907
Prelang Builder <builder@prelang.com>
2015-05-04 22:02:38 +0000

    Generated scaffolding for model 'Author' via Rails

---
+++ spec/views/authors/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "authors/new", type: :view do
+  before(:each) do
+    assign(:author, Author.new(
+      :first_name => "MyString",
+      :last_name => "MyString"
+    ))
+  end
+
+  it "renders new author form" do
+    render
+
+    assert_select "form[action=?][method=?]", authors_path, "post" do
+
+      assert_select "input#author_first_name[name=?]", "author[first_name]"
+
+      assert_select "input#author_last_name[name=?]", "author[last_name]"
+    end
+  end
+end
